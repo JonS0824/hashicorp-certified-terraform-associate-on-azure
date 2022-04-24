018c5c2ab0c9468ecff04e68fa1f5523b16d1d51
Jon Song <jon.song.2020@outlook.com>
2022-04-25 10:46:02 +1200

    Demo lab practice

---
+++ 05-Terraform-Provider-Resource-Block-Basics/Lab/resourcegroups.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "myrg1" {
+  name = "azae-tf-rg-01"
+  location = "australiaeast"
+}
--- 05-Terraform-Provider-Resource-Block-Basics/Lab/self-lab.tf
+++ 05-Terraform-Provider-Resource-Block-Basics/Lab/self-lab.tf
@@ -1,4 +1,17 @@
 # Terraform Block
 terraform {
-  required_version = ">=1.0.0"
+  required_version = "~> 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0"
+      #version = "~> 2.64"   # For production grade
+      #version = ">= 2.0.0, <= 3.3.0"    # Versions greater or equal to 2.0.0, but less or equal to 3.3.0
+     }
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}
 }
